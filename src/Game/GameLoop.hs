0d0cf95e8565dc7d3a58bb3522719d536034ce45
Anthony Willy <twilly2004@gmail.com>
2025-08-03 20:46:29 -0300

    alteracao no gameloop.hs e sokobanmap.hs para funcionar corretamente com o json

--- src/Game/GameLoop.hs
+++ src/Game/GameLoop.hs
@@ -19,10 +19,10 @@ import Game.IO
 gameLoop :: A.Array (Int, Int) Tile -> [(Int, Int)] -> (Int, Int) -> (Int, Int) -> String -> Int ->IO ()
 gameLoop gameMap markPos currentPlayerPos pushedBoxPos dificuldadeAtual level = do
     
-    --atualiza posição da caixa(se tiver sido empurrada) OQ VCS ACHAM DE TESTAR ISSO COM O PLAYER TBM?
-    let updatedMap = if pushedBoxPos /= (-1, -1) 
-                     then gameMap A.// [(currentPlayerPos, Floor), (pushedBoxPos, Box)]
-                     else gameMap
+    -- atualiza posição da caixa (se tiver sido empurrada)
+    let updatedMap = if pushedBoxPos /= (-1, -1)
+                        then gameMap A.// [(currentPlayerPos, Floor), (pushedBoxPos, Box)]
+                        else gameMap
 
     if checaVitoria updatedMap markPos
         then do
@@ -30,26 +30,28 @@ gameLoop gameMap markPos currentPlayerPos pushedBoxPos dificuldadeAtual level = 
             let proximoNivel = level + 1
             if proximoNivel >= 5
                 then do
-                    putStrLn $ "Você completou todos os níveis da dificuldade " ++ (takeBaseName dificuldadeAtual) ++ "!" --take 5 pra tirar o .json do final
+                    putStrLn $ "Você completou todos os níveis da dificuldade " ++ (takeBaseName dificuldadeAtual) ++ "!"
                     if dificuldadeAtual == "hard.json"
                         then do
                             putStrLn "Parabéns por completar o jogo!"
                             threadDelay 1000000
-                            --exitSuccess
+                            -- exitSuccess
                     else do
                         putStrLn $ "Avançando para a dificuldade " ++ (takeBaseName (proximaDificuldade dificuldadeAtual))
                         threadDelay 2000000
-                        (newGameMap, newMarks) <- loadMapFromJSON ("data/maps/" ++ (proximaDificuldade dificuldadeAtual)) 0
-                        gameLoop newGameMap newMarks (4, 4) (-1, -1) (proximaDificuldade dificuldadeAtual) 0
+                        -- Carrega o próximo nível da nova dificuldade
+                        (newGameMap, newPlayerPos, newMarks) <- loadMapFromJSON ("data/maps/" ++ (proximaDificuldade dificuldadeAtual)) 0
+                        gameLoop newGameMap newMarks newPlayerPos (-1, -1) (proximaDificuldade dificuldadeAtual) 0
             else do
                 putStrLn "Passando para o próximo nível..."
                 threadDelay 1000000
                 let proximoNivel = level + 1
-                (newGameMap, newMarks) <- loadMapFromJSON ("data/maps/" ++ dificuldadeAtual) proximoNivel
-                gameLoop newGameMap newMarks (4, 4) (-1, -1) dificuldadeAtual proximoNivel
+                -- Carrega o próximo nível da mesma dificuldade
+                (newGameMap, newPlayerPos, newMarks) <- loadMapFromJSON ("data/maps/" ++ dificuldadeAtual) proximoNivel
+                gameLoop newGameMap newMarks newPlayerPos (-1, -1) dificuldadeAtual proximoNivel
         else do
             clearScreen
-            putStrLn "=== SOKOBAN ===" 
+            putStrLn "=== SOKOBAN ==="
             putStrLn $ "Dificuldade: " ++ (takeBaseName dificuldadeAtual) ++ " | Nível: " ++ show (level + 1)
             printMap updatedMap currentPlayerPos
             putStrLn $ "Posição: " ++ show currentPlayerPos
@@ -63,17 +65,18 @@ gameLoop gameMap markPos currentPlayerPos pushedBoxPos dificuldadeAtual level = 
                 then do
                     putStrLn "Reiniciando nível"
                     threadDelay 500000
-                    (newGameMap, newMarks) <- loadMapFromJSON ("data/maps/" ++ dificuldadeAtual) level
-                    gameLoop newGameMap newMarks (4, 4) (-1, -1) dificuldadeAtual level
-
-                else 
-                    let (boxNewPos, playerNewPos) = move True tecla currentPlayerPos updatedMap
-                    in gameLoop updatedMap markPos playerNewPos boxNewPos dificuldadeAtual level
+                    -- Reinicia o nível, lendo a posição inicial do jogador do JSON
+                    (newGameMap, newPlayerPos, newMarks) <- loadMapFromJSON ("data/maps/" ++ dificuldadeAtual) level
+                    gameLoop newGameMap newMarks newPlayerPos (-1, -1) dificuldadeAtual level
+            else
+                let (boxNewPos, playerNewPos) = move True tecla currentPlayerPos updatedMap
+                in gameLoop updatedMap markPos playerNewPos boxNewPos dificuldadeAtual level
 
 -- === Início do jogo ===
 start :: String -> Int -> IO ()
 start dificuldadeAtual level = do
     cwd <- getCurrentDirectory
     let jsonPath = cwd </> "data/maps/" ++ dificuldadeAtual
-    (gameMap, marks) <- loadMapFromJSON jsonPath level
-    gameLoop gameMap marks (4, 4) (-1, -1) dificuldadeAtual level  -- (-1,-1) necessário por causa da auteração no gameLoop
+    -- Lê a posição inicial do jogador diretamente do JSON
+    (gameMap, playerPos, marks) <- loadMapFromJSON jsonPath level
+    gameLoop gameMap marks playerPos (-1, -1) dificuldadeAtual level
