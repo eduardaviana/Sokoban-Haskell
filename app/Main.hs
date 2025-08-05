107614b6d42818d375ff125eb26b09f88c4f2045
eduardaviana <mariaeduardaviana142@gmail.com>
2025-08-05 10:18:59 -0300

    feat: adiciona começo da documentação do codigo

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
+-- | Módulo principal do projeto responsável por gerenciar a navegação do menu inicial e iniciar a partida do jogo.
 module Main where
 
+-- | importação de outros modulos para utilizar suas funções
 import Game.GameLoop (start)
 import Game.IO
 import Control.Concurrent (threadDelay)
 import System.FilePath (takeBaseName)
 
+-- | Função para exibir o menu principal do jogo
 mostrarMenu :: IO()
 mostrarMenu = do
     putStrLn "███████  ██████  ██   ██  ██████  ██████   █████  ███    ██"
@@ -14,19 +17,25 @@ mostrarMenu = do
     putStrLn "███████  ██████  ██   ██  ██████  ██████  ██   ██ ██   ████"
     putStrLn ""                                               
     putStrLn "Bem-vindo ao Sokoban em Haskell!"
-    putStrLn "1. Entrar no Jogo"
-    putStrLn "2. Selecionar Dificuldade"
-    putStrLn "3. Selecionar Level"
-    putStrLn "4. Sair"
+    putStrLn " ------------------------------"
+    putStrLn "| 1.      Entrar no Jogo       |"
+    putStrLn "| 2.  Selecionar Dificuldade   |"
+    putStrLn "| 3.      Selecionar Level     |"
+    putStrLn "| 4.          Sair             |"
+    putStrLn " ------------------------------"
     putStrLn "" 
 
+-- | Função de menu para escolha de dificuldade
+-- | @return Uma String contendo o nome do arquivo JSON da dificuldade selecionada.
 dificuldade :: IO String
 dificuldade = do
     clearScreen
     putStrLn "Selecione a dificuldade:"
-    putStrLn "1. Fácil"
-    putStrLn "2. Médio"
-    putStrLn "3. Difícil"
+    putStrLn " ---------------------"
+    putStrLn "| 1.  Fácil           |"
+    putStrLn "| 2.  Médio           |"
+    putStrLn "| 3.  Difícil         |"
+    putStrLn " ---------------------"
     putStrLn ""
 
     escolha <- getLine
@@ -39,6 +48,8 @@ dificuldade = do
             threadDelay 1000000
             dificuldade
 
+-- | Função de menu para escolha de dificuldade
+-- | @return Um Int que representa o índice do level selecionado.
 level :: IO Int
 level = do
     clearScreen
@@ -56,9 +67,14 @@ level = do
             threadDelay 1000000
             level
 
+-- | Função para imprimir mensagem de saída do jogo
 quit :: IO()
-quit = putStrLn "SAIU DO JOGO"
+quit = putStrLn "OBRIGADO POR JOGAR! :)"
 
+-- | Função do menu principal para manipular as escolhas do jogador
+-- | @param estadoAtual String: A string que representa o estado atual do menu.
+-- | @param dificuldadeAtual String: A string do nome do arquivo da dificuldade atual.
+-- | @param levelAtual Int: O índice do level selecionado.
 menu :: String -> String -> Int -> IO()
 menu "options" dificuldadeAtual levelAtual = do
     clearScreen
@@ -71,6 +87,7 @@ menu "options" dificuldadeAtual levelAtual = do
             threadDelay 600000
             menu "options" dificuldadeAtual levelAtual
 
+-- | Entra diretamente no jogo chamando a função start 
 menu "1" dificuldadeAtual levelAtual = do 
     putStrLn "Iniciando o jogo..."
     threadDelay 500000
@@ -78,21 +95,26 @@ menu "1" dificuldadeAtual levelAtual = do
     threadDelay 500000
     menu "options" dificuldadeAtual levelAtual
 
+-- | Se o jogador quiser selecionar a dificuldade é chamada a função de dificuldade
 menu "2" _ levelAtual = do
     novaDificuldade <- dificuldade
     putStrLn $ "Dificuldade definida como: " ++ takeBaseName novaDificuldade
     threadDelay 500000
     menu "options" novaDificuldade levelAtual
 
+-- | Se o jogador quiser selecionar o level é chamada a função level 
 menu "3" dificuldadeAtual _ = do
     novoLevel <- level
     putStrLn $ "Level selecionado: " ++ show (novoLevel + 1)
     threadDelay 500000
     menu "options" dificuldadeAtual novoLevel
 
+-- | Se o jogador quiser sair é selecionada a função quit
 menu "4" _ _ = quit
 
+-- | O menu deve ficar aparecendo até que uma das opções acima seja escolhida
 menu _ dificuldadeAtual levelAtual = menu "options" dificuldadeAtual levelAtual 
 
+-- | Chamada para o menu principal onde o jogo inicia na dificuldade facil nivel 1
 main :: IO()
 main = menu "options" "easy.json" 0
--- src/Game/GameLoop.hs
+++ src/Game/GameLoop.hs
@@ -1,11 +1,12 @@
-{-# LANGUAGE DeriveGeneric #-}
+--{-# LANGUAGE DeriveGeneric #-}
 
+-- | Este módulo contém a lógica do loop principal do jogo responsavel por gerenciar a interação do jogador, o estado do jogo e a progressão de níveis. 
 module Game.GameLoop where
 
 import qualified Data.Array as A
-import qualified Data.ByteString.Lazy as B
-import Data.Aeson (decode)
-import System.Exit (exitFailure)
+--import qualified Data.ByteString.Lazy as B
+--import Data.Aeson (decode)
+--import System.Exit (exitFailure)
 import System.Directory (getCurrentDirectory)
 import System.FilePath ((</>), takeBaseName)
 import Control.Concurrent (threadDelay)
@@ -15,11 +16,17 @@ import Game.Logic
 import Game.SokobanMap
 import Game.IO
 
--- O loop principal do jogo agora recebe a lista de posições das marcas.
+-- Função de loop principal do jogo, que recebe como parametros: 
+-- | @param gameMap A.Array (Int, Int) Tile: O mapa atual do jogo.
+-- | @param markPos [(Int, Int)]: A lista de posições das marcas (alvos).
+-- | @param currentPlayerPos (Int, Int): A posição atual do jogador.
+-- | @param dificuldadeAtual String: A string que representa a dificuldade atual (e.g., "easy.json").
+-- | @param level Int: O índice do nível atual dentro da dificuldade (0-indexado).
 gameLoop :: A.Array (Int, Int) Tile -> [(Int, Int)] -> (Int, Int) -> String -> Int ->IO ()
 gameLoop gameMap markPos currentPlayerPos dificuldadeAtual level = do
-    
+    --Verifica se o jogador ganhou chamando a função checaVitoria do Logic
     if checaVitoria gameMap markPos
+        -- Se o jogador tiver ganhado o nível ou ele irá para o próximo da dificuldade ou completou o jogo
         then do
             putStrLn "Sucesso!"
             let proximoNivel = level + 1
@@ -42,6 +49,7 @@ gameLoop gameMap markPos currentPlayerPos dificuldadeAtual level = do
                 let proximoNivel = level + 1
                 (newGameMap, newPlayerPos, newMarks) <- loadMapFromJSON ("data/maps/" ++ dificuldadeAtual) proximoNivel
                 gameLoop newGameMap newMarks newPlayerPos dificuldadeAtual proximoNivel
+        -- Se o jogador ainda não tiver ganhado é exibido na tela o mapa com o jogador e suas coordenadas no jogo
         else do
             clearScreen
             putStrLn "=== SOKOBAN ==="
@@ -51,32 +59,41 @@ gameLoop gameMap markPos currentPlayerPos dificuldadeAtual level = do
             putStrLn $ "Posição: " ++ show currentPlayerPos
             putStrLn "Use w/a/s/d para mover, q para sair, r para reiniciar"
 
+            --ler a decisão do jogador
             tecla <- getCharInstant
 
+            -- q: sair do jogo
             if tecla == 'q'
                 then putStrLn "Fim do jogo!"
+            -- r: reiniciar nivel 
             else if tecla == 'r'
                 then do
                     putStrLn "Reiniciando nível"
                     threadDelay 500000
                     (newGameMap, newPlayerPos, newMarks) <- loadMapFromJSON ("data/maps/" ++ dificuldadeAtual) level
                     gameLoop newGameMap newMarks newPlayerPos dificuldadeAtual level
+            -- _: Tentativa de movimento no jogo (jogador/Caixa)
             else
+                -- chama a função move do Logic para calcular a nova posição
                 let (boxNewPos, playerNewPos) = move True tecla currentPlayerPos gameMap
+                -- Verifica se foi possivel modificar o jogador 
                 in if playerNewPos /= currentPlayerPos
+                    --Se o jogador conseguiu se mexer guarda sua posição antiga e os Tile da posição anterior e da nova
                    then
                        let oldPlayerPos = currentPlayerPos
                            oldPlayerTile = gameMap A.! oldPlayerPos
                            newPlayerTile = gameMap A.! playerNewPos
 
+                        -- Verifica se a nova posição do jogador é uma caixa, se sim a caixa foi empurrada.
                            isBoxPushed = newPlayerTile == Box
 
-
+                        -- Decide o tile que ficará na posição antiga do jogador, se a posição antes era uma Mark ela volta a ser marca se não o tile é Floor
                            tileAtOldPlayerPos = if oldPlayerPos `elem` markPos
                                                 then Mark
                                                 else Floor
-
+                        -- Atualiza mapa após o movimento
                            updatedMap = 
+                            -- Se a caixa foi empurrada atualiza o gameMap passando os novos parametros para os antigos Tiles do jogador e caixa e a nova posição da caixa
                                if isBoxPushed
                                    then 
                                        let oldBoxPos = playerNewPos
@@ -85,13 +102,18 @@ gameLoop gameMap markPos currentPlayerPos dificuldadeAtual level = do
                                                              else Floor
                                            newMap = gameMap A.// [(oldPlayerPos, tileAtOldPlayerPos), (oldBoxPos, tileAtOldBoxPos), (boxNewPos, Box)]
                                        in newMap
+                            -- Se nenhuma caixa foi empurrada atualiza o gameMap apenas para o antigo Tile do jogador e a nova posição do jogador
                                    else 
                                        gameMap A.// [(oldPlayerPos, tileAtOldPlayerPos), (playerNewPos, Player)]
                         in gameLoop updatedMap markPos playerNewPos dificuldadeAtual level
+                    -- Se não foi possivel modificar o jogador chama novamente o gameLoop com os mesmo parametros
                    else gameLoop gameMap markPos currentPlayerPos dificuldadeAtual level
 
 
--- === Início do jogo ===
+-- Função que inicia o jogo, recebe como parametros:
+-- | @param dificuldadeAtual String: O nome do arquivo JSON da dificuldade a ser carregada
+-- | @param level Int: O índice do nível a ser carregado
+-- | @return IO () Uma ação de I/O que inicia o loop principal do jogo.
 start :: String -> Int -> IO ()
 start dificuldadeAtual level = do
     cwd <- getCurrentDirectory
