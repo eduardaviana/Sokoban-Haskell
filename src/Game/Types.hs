62b95869ec0e5a37ee7cf5fbe3f3f40a44c68602
Jota Antonio <jamqueiroz2005@gmail.com>
2025-08-07 22:33:35 -0300

    Refactor (Types.hs): Implementa novo modelo de estados e tipos

--- src/Game/Types.hs
+++ src/Game/Types.hs
@@ -4,6 +4,7 @@
 -- | Módulo que define os tipos de dados fundamentais para o jogo
 module Game.Types where
 
+import qualified Data.Array as A
 import GHC.Generics (Generic)
 import Data.Aeson (FromJSON)
 
@@ -13,11 +14,45 @@ import Data.Aeson (FromJSON)
 -- | Eq: Permite que os valores de 'Tile' sejam comparados para igualdade ('==') ou desigualdade ('/=').
 data Tile = Wall | Floor | Box | Player | Mark deriving (Show, Eq)
 
--- | Define um tipo de dado: MapWrapper para a leitura de JSON.
-data MapWrapper = MapWrapper { tileMap :: [String] } deriving (Show, Generic)
+-- | Representa as direções de movimento possíveis para o jogador.
+-- | O 'deriving (Eq, Show)' permite que valores deste tipo sejam comparados e exibidos como String.
+data Direction = Up | Down | Left | Right deriving (Eq, Show)
 
--- | Declara que o tipo MapWrapper é uma instância da classe de tipo 'FromJSON'.
-instance FromJSON MapWrapper
+-- | Representa as ações que o jogador pode realizar durante o jogo.
+-- | Este tipo de dado melhora a segurança e clareza do código em comparação com o uso de 'Char'.
+data Action = Move Direction | Restart | Quit | NoOp deriving (Eq, Show)
+
+-- | Define a configuração de um nível. Contém dados que NÃO mudam durante uma partida.
+-- | Agrupar estes dados em um record simplifica a passagem de parâmetros entre funções.
+data GameConfig = GameConfig
+  { gcDifficulty :: String
+  , gcLevel      :: Int
+  , gcMarkPos    :: [(Int, Int)]
+  } deriving (Show)
+
+-- | Define o estado do jogo. Contém dados que MUDAM a cada movimento do jogador.
+data GameState = GameState
+  { gsMap       :: A.Array (Int, Int) Tile
+  , gsPlayerPos :: (Int, Int)
+  } deriving (Show)
+
+-- | Tipo de dado auxiliar para a decodificação de um único nível do arquivo JSON.
+data LevelLocal = LevelLocal
+  { tileMap :: [String]
+  , marks   :: [[Int]]
+  } deriving (Show, Generic)
+
+-- | Tipo de dado auxiliar que representa a estrutura completa do arquivo JSON, contendo uma lista de níveis.
+data MapWrapperLocal = MapWrapperLocal
+  { levels :: [LevelLocal]
+  } deriving (Show, Generic)
+
+-- | Declara que o tipo 'LevelLocal' é uma instância da classe 'FromJSON'.
+-- | Aeson usa a derivação 'Generic' para criar a implementação automaticamente.
+instance FromJSON LevelLocal
+
+-- | Declara que o tipo 'MapWrapperLocal' é uma instância da classe 'FromJSON'.
+instance FromJSON MapWrapperLocal
 
 -- | Função que converte um valor do tipo 'Tile' para um 'Char' correspondente, para exibição no terminal.
 -- | @param tile Tile: O tipo de tile a ser convertido.
